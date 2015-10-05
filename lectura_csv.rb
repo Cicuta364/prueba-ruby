55fc627ed2cdfcc4a8b822bbe70970b237236d71
Cicuta364 <patricio.campos.u@gmail.com>
2015-10-05 13:48:10 -0300

    mejora al código en arreglo para borrar

--- lectura_csv.rb
+++ lectura_csv.rb
@@ -5,7 +5,7 @@ require_relative 'peliculas.rb'
 peliculas = []
 csv = CSV.read('dvd_csv_2.csv', headers: true, header_converters: :symbol, converters: :numeric )
 
-%i(status sound versions aspect upc dvd_releasedate id timestamp price).each { |e| csv.delete(e) }
+[:status, :sound, :versions, :aspect, :upc, :dvd_releasedate, :id, :timestamp, :price].each { |e| csv.delete(e) }
 
 peliculas = csv.map(&:to_hash)
 
